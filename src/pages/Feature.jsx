ccdb23a4ee143717a4d6b1141e88f6216da2542e
zolatido <krizellesanchez15@gmail.com>
2023-12-07 20:07:38 +0800

    feature updated

--- src/pages/Feature.jsx
+++ src/pages/Feature.jsx
@@ -1,6 +1,12 @@
 // Import the React library for creating components.
 import React from "react";
 
+// Import the "rightimage" from the "assets" folder. Make sure the path is correct.
+import create from '../assets/Create.png';
+import deleteicon from '../assets/Delete.png';
+import read from '../assets/Read.png';
+import update from '../assets/Update.png';
+
 
 const Feature = () => {
     return (
@@ -10,21 +16,28 @@ const Feature = () => {
             <div className="w-[15rem] md:w-[25rem] border-t-[3px] border-black mb-10 mt-5" />
             <div className="flex flex-row gap-5 md:gap-10">
                 <div className="flex flex-col items-center ">
-                    <div className="w-[4rem] md:w-[6rem] h-[4rem] md:h-[6rem] rounded-full bg-gradient-to-tr from-sky-200 via-white to-blue-500 border-[1px] border-black">
-
+                    <div className="w-[4rem] md:w-[6rem] h-[4rem] md:h-[6rem]">
+                    <img src={create} />
                     </div>
                     <h1 className="text-xl uppercase font-semibold mt-3">Create</h1>
+                    
                 </div>
                 <div className="flex flex-col items-center">
-                    <div className="w-[4rem] md:w-[6rem] h-[4rem] md:h-[6rem] rounded-full bg-gradient-to-tr from-sky-200 via-white to-blue-500 border-[1px] border-black"></div>
+                    <div className="w-[2rem] md:w-[4.5rem] h-[4rem] md:h-[6rem]">
+                    <img src={read} />
+                    </div>
                     <h1 className="text-xl uppercase font-semibold mt-3">Read</h1>
                 </div>
                 <div className="flex flex-col items-center">
-                    <div className="w-[4rem] md:w-[6rem] h-[4rem] md:h-[6rem] rounded-full bg-gradient-to-tr from-sky-200 via-white to-blue-500 border-[1px] border-black"></div>
+                    <div className="w-[4rem] md:w-[6rem] h-[4rem] md:h-[6rem]">
+                    <img src={update} />
+                    </div>
                     <h1 className="text-xl uppercase font-semibold mt-3">Update</h1>
                 </div>
                 <div className="flex flex-col items-center">
-                    <div className="w-[4rem] md:w-[6rem] h-[4rem] md:h-[6rem]w-[4rem] md:h-[6rem] rounded-full bg-gradient-to-tr from-sky-200 via-white to-blue-500 border-[1px] border-black"></div>
+                    <div className="w-[4rem] md:w-[6rem] h-[4rem] md:h-[6rem]">
+                    <img src={deleteicon} />
+                    </div>
                     <h1 className="text-xl uppercase font-semibold mt-3">Delete</h1>
                 </div>
             </div>
