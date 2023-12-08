cf397aaf8e5b4a0b5e47ff55689d366bad7dde0f
zolatido <119398300+zolatido@users.noreply.github.com>
2023-12-08 20:17:25 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/XTRACK_KAIJA/",
+  base: "/XTRACK-KAIJA/",
 })
