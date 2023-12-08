9de63522b6aca37f509ff63455f351fb94d6e13c
jmvillasan <112672301+jmvillasan@users.noreply.github.com>
2023-12-08 11:34:18 +0800

    Changed for Deployment

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -13,8 +13,8 @@ const Welcome = () => {
             <div className="flex flex-col w-96 md:w-3/5 gap-10">
                 <h1 className="font-bold text-2xl md:text-6xl">Never let the bread expire again, track it before it’s too late!</h1>
                 <div className="flex flex-row">
-                    <Link to='/x-track-web/login' className="bg-[#1F487E]/[.30] px-10 py-2 rounded-xl text-xl font-bold">Track now!</Link>
-                    <Link to='/x-track-web/about' className="text-xl px-10 py-2 flex flex-row">Learn more</Link>
+                    <Link to='/XTRACK_KAIJA/login' className="bg-[#1F487E]/[.30] px-10 py-2 rounded-xl text-xl font-bold">Track now!</Link>
+                    <Link to='/XTRACK_KAIJA/about' className="text-xl px-10 py-2 flex flex-row">Learn more</Link>
                 </div>
             </div>
             <div className=" w-96 md:w-2/5 self-center">
